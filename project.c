f9a86bcef0c30f841c46ebbcb46772b947299822
Caleb Scott <calebcscott@outlook.com>
2018-11-09 13:49:42 -0600

    modified project.c

--- project.c
+++ project.c
@@ -6,9 +6,9 @@
 int main() {
 	NodeT *pRoot = insertT(pRoot, 30);
 	pRoot = insertT(pRoot, 20);
-	pRoot = insertT(pRoot, 40);
-	pRoot = insertT(pRoot, 50);
 	pRoot = insertT(pRoot, 45);
+	pRoot = insertT(pRoot, 50);
+	pRoot = insertT(pRoot, 40);
 	pRoot = insertT(pRoot, 10);
 	
 	printT(pRoot, 0);
